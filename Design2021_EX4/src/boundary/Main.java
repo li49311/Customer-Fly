1742bfc7a8a6123c39d44ba6c907de8f1baae8c2
li49311 <li49311@gmail.com>
2021-12-21 11:55:07 +0200

    no message

--- Design2021_EX4/src/boundary/Main.java
+++ Design2021_EX4/src/boundary/Main.java
@@ -1,10 +1,24 @@
 package boundary;
 
-public class Main {
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 
+public class Main extends Application {
+	
 	public static void main(String[] args) {
-		System.out.println("hey");
-
+		launch(args);
 	}
-
+	@Override
+	public void start(Stage primaryStage) throws Exception {	
+		Parent root = FXMLLoader.load(getClass().getResource("/boundary/login.fxml"));
+		Scene scene = new Scene(root,900,700);
+		primaryStage.setScene(scene);
+		primaryStage.setTitle("Customer-Fly");
+		primaryStage.setResizable(false);
+		primaryStage.centerOnScreen();
+		primaryStage.show();
+	}
 }
